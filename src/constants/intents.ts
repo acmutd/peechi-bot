4bc2cb945c17119c8cf03b182ceb27c0095d1c64
Ruben Olano <rubendavid2711@gmail.com>
2025-09-14 11:21:58 -0500

    remove unnexessary intents

--- src/constants/intents.ts
+++ src/constants/intents.ts
@@ -9,12 +9,5 @@ export const BOT_INTENTS = [
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent,
   GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.DirectMessages,
-  GatewayIntentBits.GuildModeration,
-  GatewayIntentBits.AutoModerationConfiguration,
-  GatewayIntentBits.AutoModerationExecution,
   GatewayIntentBits.GuildScheduledEvents,
-  GatewayIntentBits.GuildVoiceStates,
-  GatewayIntentBits.GuildPresences,
-  GatewayIntentBits.GuildWebhooks,
 ] as const
